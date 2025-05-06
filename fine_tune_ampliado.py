479b601efdef7915cc68fffc698f284d5d24bb51
Jaime Carvajal <jaime.carvajal@benchmarkemail.com>
2025-05-05 22:52:41 -0500

    dejando solo una epoca

--- fine_tune_ampliado.py
+++ fine_tune_ampliado.py
@@ -21,8 +21,8 @@ def get_config():
         "model_name": "all-MiniLM-L6-v2",
         "train_dataset_path": "training_data_quality.jsonl",
         "output_path": "fine_tuned_model_oil_gas",
-        "num_train_epochs": 5,
-        "batch_size": 16
+        "num_train_epochs": 1,
+        "batch_size": 32
     }
 
 def setup_logging():
